515ef08e0726acbee2bc8a55e52231ea60bf9e97
sjain23 <47904393+sjain23@users.noreply.github.com>
2020-06-08 21:18:54 -0400

    Cascading and Python file uploaded
    Two files used to detect and count faces in a camera frame.

---
+++ face_detect.py
@@ -0,0 +1,36 @@
+import cv2
+#import sys
+
+#cascading file for 
+cascPath = 'haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    retVal,frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.005,
+        minNeighbors=10,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    print(len(faces))
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    cv2.imshow('faces found', int(len(faces)))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
